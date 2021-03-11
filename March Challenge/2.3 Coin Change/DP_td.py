49107263d3d2389f2a8596b2aa2ed317c18dfcad
yanzhegeo <Yanzhe.Yin@uga.edu>
2021-03-11 14:51:03 -0500

    Update DP_td.py

--- March Challenge/2.3 Coin Change/DP_td.py
+++ March Challenge/2.3 Coin Change/DP_td.py
@@ -1,7 +1,9 @@
-dp = [float('inf')] * (amount + 1)
-        dp[0] = 0
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        @lru_cache(None)
+        def dp(i):
+            if i == 0: return 0
+            if i < 0: return float("inf")
+            return min(dp(i - coin) + 1 for coin in coins)
         
-        for coin in coins:
-            for x in range(coin, amount + 1):
-                dp[x] = min(dp[x], dp[x - coin] + 1)
-        return dp[amount] if dp[amount] != float('inf') else -1 
+        return dp(amount) if dp(amount) != float("inf") else -1
